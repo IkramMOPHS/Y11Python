ad6a38d5af89b01dd522df4a2c22d5337491779f
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-01-29 09:59:43 +0000

    Create Selection05.py

---
+++ Selection05.py
@@ -0,0 +1,13 @@
+rain = str(input("Is it raining? ")).lower()
+
+if rain == "yes":
+    wind = str(input("Is it windy? ")).lower()
+
+    if wind == "yes":
+        print("It's too windy for an umbrella")
+
+    else:
+        print("Take an umbrella")
+
+else:
+    print("Have a nice day")
