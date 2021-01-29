2345838647753d587706be5ba2c3ed5a330a134d
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-01-29 09:55:55 +0000

    Create Selection04.py

---
+++ Selection04.py
@@ -0,0 +1,8 @@
+colour = str(input("Enter your favourite colour: ")).lower()
+
+if colour == "red":
+    print("I like red too.")
+
+else:
+    print("I don't like" ,colour,"," ,colour, "is for losers")
+
