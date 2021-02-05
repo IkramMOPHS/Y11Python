a91921559af7bbde879481946a33bd12cb797774
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-05 11:48:11 +0000

    Create whileloopchallenges05.py

---
+++ whileloopchallenges05.py
@@ -0,0 +1,27 @@
+compnum = 50
+newguess = "y"
+guess = int(input("Enter a number: "))
+
+count = 1
+
+while newguess == "y":
+
+    if guess != compnum:
+        print("Incorrect guess")
+
+        if guess > compnum:
+            print("Too high")
+
+        else:
+            print("Too low")
+
+        newguess = str(input("Do you want another guess? y/n: ")).lower()
+        if newguess == "y":
+            guess = int(input("Enter a number: "))
+            count = count + 1
+
+    else:
+        print("You got it!")
+        newguess = "n"
+
+print("It took you" ,count, "times to guess the value.")
