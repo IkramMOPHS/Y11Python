5fdfc3e5fd9ab29ae7b73d3fe2f6942d2dd7080f
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-05 12:14:12 +0000

    Create whileloopchallenges07.py

---
+++ whileloopchallenges07.py
@@ -0,0 +1,25 @@
+value = 10
+
+while value != 0:
+
+    print("There are" ,value, "green bottles hanging on the wall.")
+    print(value, "green bottles hanging on the wall")
+    print("And if one green bottle should accidentally fall...")
+
+    print("")
+
+    guess = int(input("How many green bottles will be hanging on the wall? "))
+
+    if guess == value - 1:
+        print("")
+        print("There will be" ,value, "green bottles hanging on the wall.")
+        value = value - 1
+        value = guess
+
+    else:
+        print("")
+        print("Try again")
+        value = int(input("How many green bottles will be hanging on the wall? "))
+
+print("")
+print("There will be no green bottles hanging on the wall")
