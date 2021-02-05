564b3169c145fb42c1877ea6391c1eba3c9a11b4
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-05 11:30:58 +0000

    Create whileloopchallenges04.py

---
+++ whileloopchallenges04.py
@@ -0,0 +1,17 @@
+ctrl = "y"
+
+count = 0
+
+while ctrl == "y":
+
+    name = str(input("Enter a name for the party: "))
+    print(name, "has been invited.")
+
+    count = count + 1
+
+    ctrl = str(input("Do you want to invite anyone else? y/n: ")).lower()
+
+
+
+print(count, "people have been invited to the party.")
+
