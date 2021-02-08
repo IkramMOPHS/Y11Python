e3abcd2a09b1770f4fba854afa99a44658314abc
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-08 15:00:15 +0000

    Create stringslicing04.py

---
+++ stringslicing04.py
@@ -0,0 +1,8 @@
+rhyme = str(input("Enter the first line of a nursery rhyme: "))
+
+print("This has," ,len(rhyme), "letters in it")
+
+startnum = int(input("Enter a starting number: "))
+endnum = int(input("Enter a ending number: "))
+
+print(rhyme[startnum:endnum])
