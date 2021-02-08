4ef55233a934b4938ecb05c56646f037a429a8ab
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-08 14:14:54 +0000

    Create forloopschallenges07.py

---
+++ forloopschallenges07.py
@@ -0,0 +1,17 @@
+total = 0
+
+for i in range(0,5):
+
+    number = int(input("Please enter a number: "))
+
+    add = "yes"
+
+    decision = str(input("DO you want the number to be added to the total? Yes or No: ")).lower()
+
+    if decision == add:
+        total = total + number
+
+    else:
+        print("It will not be included")
+
+print(total)
