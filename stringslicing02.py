7aa77f641a2664221b3964abdb4c3df9c86967c5
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-08 14:39:17 +0000

    Create stringslicing02.py

---
+++ stringslicing02.py
@@ -0,0 +1,8 @@
+name = str(input("Enter your first name: "))
+lastname = str(input("Enter your last name: "))
+space = " "
+
+totalname = name + space + lastname
+
+print(totalname)
+print(len(totalname))
