453bef6855c5282ff89f027b452734787d7e7bc2
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-08 14:24:55 +0000

    Create forloopschallenges08.py

---
+++ forloopschallenges08.py
@@ -0,0 +1,16 @@
+direction = str(input("Which direction do you want to go? u/d: ")).lower()
+
+if direction == "u":
+    num = int(input("Enter a top number: "))
+
+    for i in range(0,num):
+        print(1+i)
+
+elif direction == "d":
+    num = int(input("Enter a bottom number less than 20: "))
+
+    for i in range(0, 21-num):
+        print(20-i)
+
+else:
+    print("I don't understand")
