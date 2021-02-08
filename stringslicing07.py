3d0dd7544bcfa1abd111b6299a40ce23c80efd09
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-08 15:11:12 +0000

    Create stringslicing07.py

---
+++ stringslicing07.py
@@ -0,0 +1,9 @@
+name = str(input("Enter your first name: ")).title()
+
+if name[0] == "A" or name[0] == "E" or name[0] == "I" or name[0] == "O" or name[0] == "U":
+
+    name = " "
+    print(name)
+
+else:
+    print(name)
