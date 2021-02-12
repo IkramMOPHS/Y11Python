798d0511c4c9590aa07e33e538c3ba6073839523
IkramMOPHS <71496892+IkramMOPHS@users.noreply.github.com>
2021-02-12 09:28:22 +0000

    Create stringslicing08.py

---
+++ stringslicing08.py
@@ -0,0 +1,17 @@
+loop = 0
+
+loopcounter = "y"
+
+while loopcounter == "y":
+    lastname = str(input("Enter your last name: ")).title()
+
+    loop = loop + 1
+
+    loop = str(loop)
+
+    print(lastname[0:3] + loop)
+
+    loopcounter = str(input("Any more membership numbers? y/n: ")).lower()
+
+    loop = int(loop)
+   
